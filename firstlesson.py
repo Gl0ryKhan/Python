7b64991de98f4f58a36eb61eea4b1dab1d3f1860
Erbolot <erbolotermamatov@gmail.com>
2025-10-23 20:27:23 +0600

    now I am learning how to push, commit, add st git

---
+++ firstlesson.py
@@ -0,0 +1,101 @@
+
+# # int 
+# age = 20
+# year = 2025 
+
+# #float
+# pi = 3.14
+# e = 2.7 
+
+# #complex
+# z = 3 + 4j
+
+# #string (str)
+# name = "Python"
+# message = "Hello, World"
+# multiline = """" This 
+# multiline
+# text"""
+
+# #list 
+# numbers = [1, 2, 3, 4, 5]
+# mimxed = [1, "hi", 3.14, True]
+
+# #tuple
+# coordinates = (10, 20)
+# rgb = (255, 128, 0)
+
+# #dictionary (dict)
+# person = {
+#     "name" : "Anna",
+#     "age" : 25,
+#     "city" : "Moscow"
+# }
+
+# #set 
+# unique_numbers = {1, 2, 3, 4, 5}
+
+# ###########
+
+# x = 5
+
+
+# x = "hello"
+
+
+# x = [1, 2]
+# print (type (x))
+
+
+# print ( "Line ") 
+# print (f"My name is {name} and I am {age}")
+
+# numbers = [1, 2, 3]
+# print("Список:", numbers)
+# print("Число:", 42)
+# print("Булево значение:", True)
+
+# import math as mt 
+
+
+
+
+# question_1 = input( "What is your name ?")
+
+# question_2 = input( "Where are from? ")
+
+
+
+# print("His name is", question_1)
+# print("He is from", question_2)
+
+# num_1 = int(input("Enter the first number"))
+# num_2 = int(input("Enter the second number"))
+# print(type(num_1))
+# print(type(num_2))
+
+# print ("The sum of numbers", num_1 + num_2)
+# print ("The substraction", num_1 - num_2)
+# print ("The multiply of numbers", num_1 * num_2)
+# print ("The division", num_1 / num_2)
+
+# age = 20
+# pi = 3.14
+# name = "Python"
+# print(type(age))
+# print(type(pi))
+# print(type(name))
+
+num1 = "5"
+num2 = 10
+num3 = 6.5
+
+dif = num2 - num3
+
+dif2 = num2 * num1
+
+print (dif)
+print (dif2)
+
+
+print (10==10)
