223d9c6a0e1d8b580ff5f872c393af36f167f186
Erbolot <erbolotermamatov@gmail.com>
2025-10-24 21:09:43 +0600

    friday commit

--- firstlesson.py
+++ firstlesson.py
@@ -60,42 +60,5 @@
 
 
 
-# question_1 = input( "What is your name ?")
-
-# question_2 = input( "Where are from? ")
 
 
-
-# print("His name is", question_1)
-# print("He is from", question_2)
-
-# num_1 = int(input("Enter the first number"))
-# num_2 = int(input("Enter the second number"))
-# print(type(num_1))
-# print(type(num_2))
-
-# print ("The sum of numbers", num_1 + num_2)
-# print ("The substraction", num_1 - num_2)
-# print ("The multiply of numbers", num_1 * num_2)
-# print ("The division", num_1 / num_2)
-
-# age = 20
-# pi = 3.14
-# name = "Python"
-# print(type(age))
-# print(type(pi))
-# print(type(name))
-
-num1 = "5"
-num2 = 10
-num3 = 6.5
-
-dif = num2 - num3
-
-dif2 = num2 * num1
-
-print (dif)
-print (dif2)
-
-
-print (10==10)
