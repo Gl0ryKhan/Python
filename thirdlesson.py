cd4785574b7822f13454ef78877cd39af56a7b7f
Erbolot <erbolotermamatov@gmail.com>
2025-10-27 20:56:04 +0600

    27/10/2025 commit, don't forget to learn new command

--- thirdlesson.py
+++ thirdlesson.py
@@ -1,56 +1,15 @@
-# products = ["eggs", "milk", "bread", "banana", "chokolate", 10, 25.3, 44, "Check = true", "KEY" ]
-# print (f" I wanna buy {products[1]}, it costs {products[-4] } i gave {products[-3]} and took {products[- 3] - products[-4]} ")
-# a = int(input())
-
-
-a = 2
-b = 3
-c = 1 
-sum_1 = a * ( b - c )
-print(sum_1)
-
-a = 5
-b = 1 
-sum_2 = ( a -  b)*( a + b)
-print (sum_2)
-
-a = 0.3
-b = 4 
-c = 1 
-sum_3 = a * (b - c)
-print(sum_3)
-
-a = 91 
-b = 1 
-c = 2 
-sum_4 = ( a - b) / ( c + b)
-print(sum_4)
-
 import math as mt
-a = 5
-b = 4 
-c = 4 
-sum_5 = mt.sqrt((5*4) / 3 )
-print(sum_5)
 
-a = 1
-b = 3
-c = 5 
-d = 0.2
-e = 0.001
-f = 91 
-sum_6 = a/b + c * ((d - e) / f)
-print(sum_6)
+student1 = ["Tom", "Sam", "Jack", "Bob", "Tim", "Sara"]
 
-a = 0.16
-b = 3.2 
-c = 3/40
-d = 2*(3/11)
-e = 4.125
-f = 3*(3/4)
-g = 5*(1/6)
-h = 0.3
-j = 4.5
-k = 1/3 * 0.3
-sum_7 = (a * ( b - c) + d * e / f) / ( g * h - h * j + k)
-print (sum_7)
+student2 = ["Alice", "John", "Taylor"]
+
+
+
+tim_index = student1.index("Tim")
+student1[tim_index] = "Timothy"
+
+print(student1)
+
+
+
