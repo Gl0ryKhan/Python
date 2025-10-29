2caa36c8dab1b8d7789601030b2f906b84359193
Erbolot <erbolotermamatov@gmail.com>
2025-10-29 20:58:35 +0600

    29/10/2025 commit

---
+++ fifth_lesson.py
@@ -0,0 +1,22 @@
+a = 32 
+b = 3.5 
+x = True
+
+a = str(a)
+b = str(b)
+x = str(x)
+print(type(a))
+print(type(b))
+print(type(x))
+
+
+my_string = input("Enter a text: ")
+print(my_string)
+
+name = input("Write your name: ")
+surname = input("Write your surname: ")
+print("Your name is", name)
+print("Your surname is", surname)
+
+res = name + ' ' + surname
+print(res)
